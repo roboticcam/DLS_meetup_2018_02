dab9a20f6f10b217c7ea5cb6a2fde76e751c110a
Prof Richard Xu 徐亦达教授 <xuyida@hotmail.com>
2018-02-27 02:26:11 +1100

    Add files via upload

---
+++ LSTM_with_LN.py
@@ -0,0 +1,206 @@
+import numpy as np
+import tensorflow as tf
+import time
+import os
+# import urllib.request
+from urllib2 import urlopen
+import reader
+
+from tensorflow.python.ops import rnn_cell_impl
+linear = rnn_cell_impl._linear
+file_name = 'data.txt'
+
+with open(file_name,'r') as f:
+    raw_data = f.read()
+    print("Data length:", len(raw_data))
+
+vocab = set(raw_data)
+vocab_size = len(vocab)
+idx_to_vocab = dict(enumerate(vocab))
+vocab_to_idx = dict(zip(idx_to_vocab.values(), idx_to_vocab.keys()))
+
+data = [vocab_to_idx[c] for c in raw_data]
+
+# create iterator through training data by batch size
+def gen_epochs(n, num_steps, batch_size):
+    for i in range(n):
+        yield reader.ptb_iterator(data, batch_size, num_steps)
+
+def reset_graph():
+    if 'sess' in globals() and sess:
+        sess.close()
+    tf.reset_default_graph()
+
+def ln(tensor, scope = None, epsilon = 1e-5):
+    """ Layer normalizes a 2D tensor along its second axis """
+    assert(len(tensor.get_shape()) == 2)
+    m, v = tf.nn.moments(tensor, [1], keep_dims=True)
+    if not isinstance(scope, str):
+        scope = ''
+    with tf.variable_scope(scope + 'layer_norm'):
+        scale = tf.get_variable('scale',
+                                shape=[tensor.get_shape()[1]],
+                                initializer=tf.constant_initializer(1))
+        shift = tf.get_variable('shift',
+                                shape=[tensor.get_shape()[1]],
+                                initializer=tf.constant_initializer(0))
+    LN_initial = (tensor - m) / tf.sqrt(v + epsilon)
+
+    return LN_initial * scale + shift
+
+class LayerNormalizedLSTMCell(tf.nn.rnn_cell.RNNCell):
+    """
+    Adapted from TF's BasicLSTMCell to use Layer Normalization.
+    Note that state_is_tuple is always True.
+    """
+
+    def __init__(self, num_units, forget_bias=1.0, activation=tf.nn.tanh):
+        self._num_units = num_units
+        self._forget_bias = forget_bias
+        self._activation = activation
+
+    @property
+    def state_size(self):
+        return tf.nn.rnn_cell.LSTMStateTuple(self._num_units, self._num_units)
+
+    @property
+    def output_size(self):
+        return self._num_units
+
+    def __call__(self, inputs, state, scope=None):
+        """Long short-term memory cell (LSTM)."""
+        with tf.variable_scope(scope or type(self).__name__):
+            c, h = state
+
+            # change bias argument to False since LN will add bias via shift
+            concat =  linear([inputs, h], 4 * self._num_units, False)
+
+            # i, j, f, o = tf.split(1, 4, concat)  //old
+            i, j, f, o = tf.split(concat, 4, 1)
+
+            # add layer normalization to each gate
+            i = ln(i, scope = 'i/')# input gate
+            j = ln(j, scope = 'j/')# transform gate
+            f = ln(f, scope = 'f/')# forget gate
+            o = ln(o, scope = 'o/')# output gate
+
+            new_c = (c * tf.nn.sigmoid(f + self._forget_bias) + tf.nn.sigmoid(i) *
+                   self._activation(j))
+
+            # add layer_normalization in calculation of new hidden state
+            new_h = self._activation(ln(new_c, scope = 'new_h/')) * tf.nn.sigmoid(o)
+            new_state = tf.nn.rnn_cell.LSTMStateTuple(new_c, new_h)
+
+            return new_h, new_state
+
+def build_graph(
+    cell_type = None,
+    num_weights_for_custom_cell = 5,
+    state_size = 100,
+    num_classes = vocab_size,
+    batch_size = 32,
+    num_steps = 200,
+    num_layers = 1,
+    build_with_dropout=False,
+    learning_rate = 1e-4):
+
+    reset_graph()
+
+    x = tf.placeholder(tf.int32, [batch_size, num_steps], name='input_placeholder')
+    y = tf.placeholder(tf.int32, [batch_size, num_steps], name='labels_placeholder')
+
+    dropout = tf.constant(1.0)
+
+    embeddings = tf.get_variable('embedding_matrix', [num_classes, state_size])
+
+    rnn_inputs = tf.nn.embedding_lookup(embeddings, x)
+
+    if cell_type == 'Custom':
+        cell = CustomCell(state_size, num_weights_for_custom_cell)
+    
+    elif cell_type == 'LSTM':
+        cell = tf.nn.rnn_cell.LSTMCell(state_size, state_is_tuple=True)
+    elif cell_type == 'LN_LSTM':
+        cell = LayerNormalizedLSTMCell(state_size)
+    else:
+        cell = tf.nn.rnn_cell.BasicRNNCell(state_size)
+
+    if build_with_dropout:
+        cell = tf.nn.rnn_cell.DropoutWrapper(cell, input_keep_prob=dropout)
+
+    if cell_type == 'LSTM' or cell_type == 'LN_LSTM':
+        cell = tf.nn.rnn_cell.MultiRNNCell([cell] * num_layers, state_is_tuple=True)
+    else:
+        cell = tf.nn.rnn_cell.MultiRNNCell([cell] * num_layers)
+
+    if build_with_dropout:
+        cell = tf.nn.rnn_cell.DropoutWrapper(cell, output_keep_prob=dropout)
+
+    init_state = cell.zero_state(batch_size, tf.float32)
+    rnn_outputs, final_state = tf.nn.dynamic_rnn(cell, rnn_inputs, initial_state=init_state)
+
+    with tf.variable_scope('softmax'):
+        W = tf.get_variable('W', [state_size, num_classes])
+        b = tf.get_variable('b', [num_classes], initializer=tf.constant_initializer(0.0))
+
+    #reshape rnn_outputs and y
+    rnn_outputs = tf.reshape(rnn_outputs, [-1, state_size])
+    y_reshaped = tf.reshape(y, [-1])
+
+    logits = tf.matmul(rnn_outputs, W) + b
+
+    predictions = tf.nn.softmax(logits)
+
+    total_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y_reshaped))
+    train_step = tf.train.AdamOptimizer(learning_rate).minimize(total_loss)
+
+    return dict(
+        x = x,
+        y = y,
+        init_state = init_state,
+        final_state = final_state,
+        total_loss = total_loss,
+        train_step = train_step,
+        preds = predictions,
+        saver = tf.train.Saver()
+    )
+
+def train_network(g, num_epochs, num_steps = 200, batch_size = 32, verbose = True, save=False):
+    tf.set_random_seed(2345)
+    with tf.Session() as sess:
+        sess.run(tf.initialize_all_variables())
+        training_losses = []
+        for idx, epoch in enumerate(gen_epochs(num_epochs, num_steps, batch_size)):
+            training_loss = 0
+            steps = 0
+            training_state = None
+    
+            for X, Y in epoch:
+                steps += 1
+
+                feed_dict={g['x']: X, g['y']: Y}
+                if training_state is not None:
+                    feed_dict[g['init_state']] = training_state
+                training_loss_, training_state, _ = sess.run([g['total_loss'],
+                                                      g['final_state'],
+                                                      g['train_step']],
+                                                             feed_dict)
+                training_loss += training_loss_
+            if verbose:
+                print("Average training loss for Epoch", idx, ":", training_loss/steps)
+            training_losses.append(training_loss/steps)
+            eachepoch_time = time.time()-starttime
+            print ("Totaltime form start to now: " ,  eachepoch_time)
+        
+        if isinstance(save, str):
+            g['saver'].save(sess, save)
+
+    return training_losses
+
+g = build_graph(cell_type='LSTM', num_steps=50)
+starttime = time.time()
+losses = train_network(g, 20, num_steps=50, save="saves/LSTM_20_epochs")
+
+# g = build_graph(cell_type='LN_LSTM', num_steps=50)
+# starttime = time.time()
+# losses = train_network(g, 20, num_steps=50, save="saves/LSTM_LN_20_epochs")
---
+++ dogs_cats_withoutbn.py
@@ -0,0 +1,167 @@
+import pandas as pd
+from torch import np 
+import os
+from PIL import Image
+import torch
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+from sklearn.preprocessing import MultiLabelBinarizer
+import cv2
+import time
+
+def prepare_sequence(seq, to_ix):
+    idxs = [to_ix[w] for w in seq]
+    tensor = torch.LongTensor(idxs)
+    return Variable(tensor)
+
+
+tag_to_ix = {"dog": 0, "cat": 1}
+IMG_PATH = 'train/'
+IMG_EXT = '.jpg'
+TRAIN_DATA = 'train.csv'
+class dogsandcats(Dataset):
+    def __init__(self,csv_path,img_path,img_ext,transform=None):
+        tmp_df=pd.read_csv(csv_path,sep="   ")     
+        self.mlb=MultiLabelBinarizer()
+        self.img_path=img_path
+        self.img_ext =img_ext
+        self.transform=transform
+        self.X_train=tmp_df['image_name']
+        self.y_train = tmp_df['tags'].str.split()
+    def __getitem__(self,index):
+        img = cv2.imread(self.X_train[index ])
+        img = cv2.resize(img,(227,227))
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        img = Image.fromarray(img)
+
+        if self.transform is not None:
+            img=self.transform(img)
+        label=self.y_train[index]
+        return img,label
+    def __len__(self):
+        return len(self.X_train.index)
+transformations=transforms.Compose([transforms.Scale(227),transforms.ToTensor()])
+dest_train=dogsandcats(TRAIN_DATA,IMG_PATH,IMG_EXT,transformations)
+train_loader=DataLoader(dest_train,
+                        batch_size=256,
+                        shuffle=True,
+                        num_workers=1,
+                        pin_memory=True
+                        )
+class ConvNet(torch.nn.Module):
+    def __init__(self, output_dim):
+            
+        super(ConvNet, self).__init__()
+        self.conv1=nn.Sequential(                
+                nn.Conv2d(3, 96, kernel_size=11, stride=4),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3,stride=2)
+                )
+        self.conv2=nn.Sequential(
+                nn.Conv2d(96,256,kernel_size=5,padding=2),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3,stride=2)
+                )
+        self.conv3=nn.Sequential(
+                nn.Conv2d(256,384,kernel_size=3,padding=1),
+                nn.ReLU(inplace=True)
+                )
+        self.conv4=nn.Sequential(
+                nn.Conv2d(384,384,kernel_size=3,padding=1),
+                nn.ReLU(inplace=True)
+                )
+        self.conv5=nn.Sequential(
+                nn.Conv2d(384,256,kernel_size=3,padding=1),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3,stride=2)
+                )
+        self.fc = nn.Sequential()
+        # self.fc.add_module("dropout_1",nn.Dropout(p=0.5))
+        self.fc.add_module("fc1", nn.Linear(256*6*6, 4096))
+        self.fc.add_module("relu_6",nn.ReLU())
+        # self.fc.add_module("dropout_2",nn.Dropout(p=0.5))
+        self.fc.add_module("fc2", nn.Linear(4096,4096))
+        self.fc.add_module("relu_7",nn.ReLU())
+        self.fc.add_module("fc3", nn.Linear(4096,output_dim))
+
+    def forward(self, x):
+        x=self.conv1(x)
+        x=self.conv2(x)
+        x=self.conv3(x)
+        x=self.conv4(x)
+        x=self.conv5(x)
+        x = x.view(x.size(0), -1)       
+        x = self.fc.forward(x)
+        # pred = F.sigmoid(x)
+        return x
+n_classes = 2
+model = ConvNet(output_dim=n_classes)
+model.cuda()
+
+
+def get_n_params(model):
+    pp = 0
+    for p in list(model.parameters()):
+        nn = 1
+        for s in list(p.size()):
+            nn = nn * s
+        pp += nn
+    return pp
+
+
+print get_n_params(model)
+optimizer=optim.SGD(model.parameters(),lr=0.05,momentum=0.8)
+
+
+losslist = []
+
+txt_file = open("nonbn_loss.txt", "w")
+loss_func = nn.CrossEntropyLoss()
+def train(epoch):
+    model.train()
+    totalloss = 0
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data = data.cuda(async=True)
+
+        # data=data.long()
+        target = prepare_sequence(target[0], tag_to_ix)
+        target = target.cuda(async=True)
+        data = Variable(data)
+        # target = Variable(target)
+        optimizer.zero_grad()
+        output = model(data)
+        # print(output, target)
+        # print type(output),type(target)
+        # print target
+        loss = loss_func(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx%10 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.data[0]))
+        totalloss += loss.data[0]
+        batchnum = batch_idx
+    average_loss = totalloss / batchnum
+    losslist.append(average_loss)
+    print('Train Epoch: {} \tLoss: {:.6f}'.format(epoch, average_loss))
+    return average_loss
+
+totaltime = 0
+for epoch in range(100):
+    timestart = time.time()
+    loss = train(epoch)
+    timeend = time.time()
+    timelen = timeend - timestart
+    totaltime = totaltime + timelen
+    txt_file.write(str(epoch)+ "   " + str(loss) + "   " + str(totaltime) + "\n")
+    if(loss < 0.02):
+        break
+txt_file.close()
+
+
